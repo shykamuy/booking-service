141e286c60d3f0e0072495be346b0a6d20151fb6
Arthur <50771289+shykamuy@users.noreply.github.com>
2024-10-22 20:06:51 +0500

    Add files via upload

---
+++ BookingStatisticsService/src/main/java/com/example/BookingStatisticsService/BookingStatisticsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookingStatisticsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingStatisticsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingStatisticsServiceApplication.class, args);
+	}
+
+}
---
+++ BookingStatisticsService/src/test/java/com/example/BookingStatisticsService/BookingStatisticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookingStatisticsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingStatisticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
